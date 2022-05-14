9589c5b8ef3282da20dc334aad3115e39d5108e4
Mateusz Wronka <matwro2001@gmail.com>
2022-05-14 17:51:46 +0200

    counting fish, removing noise from image

--- zad_1.py
+++ zad_1.py
@@ -1,7 +1,7 @@
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
-from skimage.color import rgb2gray
+
 sensitivity = 0.05
 
 galia = np.array(Image.open("Lab9_galia.png").convert('L'))
@@ -9,12 +9,7 @@ galia = 255-galia
 imp = np.array(Image.open("Lab9_galia_e.png").convert('L'))
 imp = 255-imp
 
-plt.imshow(galia, cmap='Greys')
-plt.show()
-plt.imshow(imp, cmap='Greys')
-plt.show()
-
-C = np.real(np.fft.ifft2(np.fft.fft2(galia) * np.fft.fft2(np.rot90(imp, 2), (463, 953))))
+C = np.real(np.fft.ifft2(np.fft.fft2(galia) * np.fft.fft2(np.rot90(imp, 2), galia.shape)))
 
 indices = np.nonzero(C > np.amax(C)*(1-sensitivity))
 x, y = [], []
@@ -22,8 +17,37 @@ for i in range(len(indices[0])):
     y.append(indices[0][i])
     x.append(indices[1][i])
 
+letter_num = len(x)
 
 plt.imshow(galia, cmap='Greys')
+plt.plot(x,y, 'ro')
+
+plt.show()
+
+
+sensitivity = 0.20
+
+school_img = Image.open("Lab9_school.jpg").convert('L')
+school = np.array(school_img)
+fish_img = school_img.crop((227, 296, 250, 320))
+fish = np.array(fish_img)
+
+plt.imshow(school, cmap='Greys')
+plt.show()
+plt.imshow(fish, cmap='Greys')
+plt.show()
+
+C = np.real(np.fft.ifft2(np.fft.fft2(school) * np.fft.fft2(np.rot90(fish, 2), school.shape)))
+
+indices = np.nonzero(C > np.amax(C)*(1-sensitivity))
+x, y = [], []
+for i in range(len(indices[0])):
+    y.append(indices[0][i])
+    x.append(indices[1][i])
+
+fish_num = len(x)
+
+plt.imshow(school, cmap='Greys')
 plt.plot(x,y, 'ro')
 
 plt.show()
--- zad_2.py
+++ zad_2.py
@@ -1,38 +1,50 @@
 import numpy as np
 from PIL import Image
 import matplotlib.pyplot as plt
-import os
 import string
 
 sensitivity = 0.01
-letter_size = 0.3
+letter_size = 0.50
+
+space_len = 40
+line_height = 30
 
 filename = "./test.PNG"
 font_path = "./font"
 
 text = np.array(Image.open(filename).convert('L'))
 text = 255 - text
+
+k = 300
+U, E, VT = np.linalg.svd(text)
+text = np.zeros((len(U), len(VT)), dtype=float)
+for i in range(k):
+    text += E[i] * np.outer(U.T[i], VT[i])
+
 plt.imshow(text, cmap='Greys')
 plt.show()
 
-letters = list(string.ascii_letters)+list(string.punctuation)[:17]+list(string.digits)
+letters = list(string.ascii_lowercase)
+letter_coords = []
 
 for l in letters:
-    f = os.path.join(font_path, l+".png")
+    f = font_path + "/" + l + ".png"
     letter = Image.open(f).convert('L')
     letter = letter.resize((int(letter.width*letter_size), int(letter.height*letter_size)))
     letter = np.array(letter)
     letter = 255 - letter
 
-
-    plt.imshow(letter, cmap='Greys')
+    plt.imshow(letter)
     plt.show()
 
     C = np.real(np.fft.ifft2(np.fft.fft2(text) * np.fft.fft2(np.rot90(letter, 2), text.shape)))
 
-    print(np.amax(C))
 
     indices = np.nonzero(C >= (1-sensitivity)*np.amax(C))
+
+    for i in range(len(indices[0])):
+        letter_coords.append((indices[0][i], indices[1][i], l))
+
     x, y = [], []
     for i in range(len(indices[0])):
         y.append(indices[0][i])
@@ -41,5 +53,21 @@ for l in letters:
 
     plt.imshow(text, cmap='Greys')
     plt.plot(x,y, 'ro')
-
     plt.show()
+
+
+letter_coords.sort()
+
+output = open("output.txt",'w')
+prev_x = letter_coords[0][1]
+prev_y = letter_coords[0][0]
+for y, x, letter in letter_coords:
+    if y-prev_y >= line_height:
+        output.write("\n")
+    elif x-prev_x >= space_len:
+        output.write(" ")
+    output.write(letter)
+    prev_x = x
+    prev_y = y
+
+output.close()
