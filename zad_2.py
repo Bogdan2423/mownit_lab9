5a6aad3b50d4e0adde9ecc253e929fcca42b0526
Mateusz Wronka <matwro2001@gmail.com>
2022-05-19 14:58:12 +0200

    working text recognition

--- zad_2.py
+++ zad_2.py
@@ -5,26 +5,26 @@ import string
 
 from skimage.feature import peak_local_max
 
-sensitivity = 0.01
-letter_size = 0.50
+sensitivity = 0.03
+letter_size = 0.62
 
-space_len = 40
-line_height = 30
+space_len = 50
+line_height = 32
 
 filename = "./test.PNG"
-font_path = "./font"
+font_path = "./CourierPrime"
 
 text = np.array(Image.open(filename).convert('L'))
 text = 255 - text
 
 k = 300
-U, E, VT = np.linalg.svd(text)
-text = np.zeros((len(U), len(VT)), dtype=float)
-for i in range(k):
-    text += E[i] * np.outer(U.T[i], VT[i])
+#U, E, VT = np.linalg.svd(text)
+#text = np.zeros((len(U), len(VT)), dtype=float)
+#for i in range(k):
+#    text += E[i] * np.outer(U.T[i], VT[i])
 
-plt.imshow(text, cmap='Greys')
-plt.show()
+#plt.imshow(text, cmap='Greys')
+#plt.show()
 
 letters = list(string.ascii_lowercase)
 letter_coords = []
@@ -36,26 +36,30 @@ for l in letters:
     letter = np.array(letter)
     letter = 255 - letter
 
-    plt.imshow(letter)
-    plt.show()
+    #plt.imshow(letter)
+    #plt.show()
 
     C = np.real(np.fft.ifft2(np.fft.fft2(text) * np.fft.fft2(np.rot90(letter, 2), text.shape)))
 
-    coordinates = peak_local_max(C, min_distance=10, threshold_rel=0.99)
+    coordinates = peak_local_max(C, min_distance=10, threshold_rel=1-sensitivity)
 
     for i in range(len(coordinates)):
-        letter_coords.append((coordinates[i][1], coordinates[i][0], l))
+        letter_coords.append((coordinates[i][0], coordinates[i][1], l))
 
-    plt.imshow(text, cmap='Greys')
-    plt.plot(coordinates[:, 1], coordinates[:, 0], 'r.')
-    plt.show()
+    #plt.imshow(text, cmap='Greys')
+    #plt.plot(coordinates[:, 1], coordinates[:, 0], 'r.')
+    #plt.show()
 
+
+for i in range(line_height, text.shape[0], line_height):
+    for j in range(len(letter_coords)):
+        if i > letter_coords[j][0] >= i-line_height:
+            letter_coords[j] = (i-line_height, letter_coords[j][1], letter_coords[j][2])
 
 letter_coords.sort()
-
 output = open("output.txt",'w')
-prev_x = letter_coords[0][1]
 prev_y = letter_coords[0][0]
+prev_x = letter_coords[0][1]
 for y, x, letter in letter_coords:
     if y-prev_y >= line_height:
         output.write("\n")
